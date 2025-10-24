42fcfc48c0cb486b9bc91ecc442964f58b6f247d
GIELCIE-AI <kisembocollins46@gmail.com>
2025-10-24 13:33:26 +0300

    config.js

---
+++ config.js
@@ -0,0 +1,78 @@
+//#ENJOY BRO😍
+// Credit: COLLINS by      
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
+global.audio = "";
+global.video = "";
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "infocollsman@gmail.com";
+global.location = "kampala, uganda";
+global.mongodb = process.env.MONGODB_URL || "mongodb+srv://Rayan:<Emmanuel237>@cluster0.8twd0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.allowJids = process.env.ALLOW_JID || "null";
+global.blockJids = process.env.BLOCK_JID || "null"; 
+global.DATABASE_URL = process.env.DATABASE_URL || "postgresql://testbot_2m27_user:Az7LOxZBevfQ7qiZ2aKDwQ4325uumm4v@dpg-crngb4o8fa8c738fs4b0-a.oregon-postgres.render.com/testbot_2m27";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "America/uganda";
+global.github = process.env.GITHUB || "https://github.com/GIELCIE-AI/voltage-md";
+
+
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/vRR63CN.jpeg,https://i.imgur.com/ovCfp9J.jpeg,https://i.imgur.com/edzo5dj.jpeg";
+global.devs = "https://wa.me/256761176257 , https://wa.me/256744866509";
+
+global.owner = process.env.OWNER_NUMBER || "PUT YOUR OWNER NUMBER";
+global.style = process.env.STYLE || "3";
+global.gdbye = process.env.GOODBYE || "false";
+global.wlcm = process.env.WELCOME || "true";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+global.disablegroup = process.env.DISABLE_GROUPS || "false",
+global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
+global.userImages = process.env.USER_IMAGES || "https://i.imgur.com/yBSoUuw.jpeg,https://i.imgur.com/XrUDzUl.jpeg,https://i.imgur.com/edjZiEx.jpeg,https://i.imgur.com/cdS2R7I.jpeg,https://i.imgur.com/u9FxdwH.jpeg,https://i.imgur.com/HEFIrto.jpeg,https://i.imgur.com/Fc0gIpE.jpeg,https://i.imgur.com/iaJtsma.jpeg,https://i.imgur.com/17yE9Uy.jpeg,https://i.imgur.com/ERGVvpZ.png"
+global.waPresence = process.env.WAPRESENCE || " ";
+global.readcmds = process.env.READ_COMMAND || "true";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "";
+global.read_status_from = process.env.READ_STATUS_FROM || "";
+
+global.api_smd = "https://drk-tech-2.onrender.com/;
+global.scan = "https://drk-tech-2.onrender.com/";
+
+global.SESSION_ID =
+  process.env.SESSION_ID ||
+  "mettre votre session ici"
+module.exports = {
+  menu: process.env.MENU || "1",
+  HANDLERS: process.env.PREFIX || ".",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.0.0",
+  caption: process.env.CAPTION || "`BRAND PRODUCT OF GIELCIE`",
+  author: process.env.PACK_AUTHER || " 𝙎-𝙏𝞢𝞜",
+  packname: process.env.PACK_NAME || "VOLTAGE-MD",
+  botname: process.env.BOT_NAME || "VOLTAGE-MD",
+  ownername: process.env.OWNER_NAME || "🌹⃟KISEMBO COLLINS",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "tCxobE1FqJgfbDZgsYQNHBFB",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "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",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "...d336",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  LANG: (process.env.THEME || "STEN").toUpperCase(),
+};
+global.rank = "updated";
+global.isMongodb = false;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+}); 
